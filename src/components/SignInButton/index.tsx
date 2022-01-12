019c0423e3e273619553052ea5fa4e45e83704e3
Alisson Silva <alissonjosef@gmail.com>
2022-01-12 16:50:59 -0300

    auth github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,26 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut,useSession } from "next-auth/react"
+
 import styles from './styles.module.scss'
 
 const SignInButton: React.FC = () => {
 
-    const isUserLoggedIn = true
+  const { data: session } = useSession()
+  console.log("🚀 ~ session", session)
     
-  return isUserLoggedIn ? (
+  return session  ? (
     <button 
+    onClick={() => signOut()}
     className={styles.signInButton}
     type="button">
         <FaGithub color='#04d361'/>
-      Alisson Silva
+      {session.user.name}
         <FiX color='#737380' className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
+    onClick={() => signIn('github')}
     className={styles.signInButton}
     type="button">
         <FaGithub color='#eba417'/>
