86b6a53fcfb197053a1cd0783465d23166bbf00f
Alisson Silva <alissonjosef@gmail.com>
2022-01-13 21:00:05 -0300

    Trabalho com CMS

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ReactElement, cloneElement } from "react";
+
+interface ActiveProps extends LinkProps {
+    children: ReactElement
+    activeClassName: string
+}
+
+export function ActiveLink({children, activeClassName, ...rest}: ActiveProps) {
+
+    const { asPath } = useRouter() 
+
+    const className = asPath === rest.href
+        ? activeClassName
+        : ''
+    
+    return (
+        <Link {...rest}>
+            {cloneElement(children,{
+                className,
+            })}
+        </Link>
+    )
+}
